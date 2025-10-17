dd141891d81c66ed5f7a1ce9d02018bc8c8c63d5
Oleksii Krylov <lehakrylov1996@gmail.com>
2025-10-17 09:19:02 +0200

    03 Done

--- 03/app.js
+++ 03/app.js
@@ -2,4 +2,24 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     console.log('DOM');
+
+    const button = document.querySelector('button');
+    const span = document.querySelector('span');
+
+    button.addEventListener('click', () => {
+        fetch('https://api.ipify.org?format=json')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Błąd sieci!');
+                }
+                return response.json();
+            })
+            .then(data => {
+                span.textContent = data.ip;
+            })
+            .catch(err => {
+                console.error(err);
+                span.textContent = 'błąd pobierania';
+            });
+    });
 }
