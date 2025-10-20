b3d94f615b9f79d776f1912046806aee4e889fc7
Oleksii Krylov <lehakrylov1996@gmail.com>
2025-10-20 16:55:12 +0200

    05 Done

--- 05/app.js
+++ 05/app.js
@@ -4,7 +4,9 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     loadUsers();
+    initAddUserForm();
 }
+
 
 function loadUsers() {
     const promise = fetchGet(apiUrl);
@@ -17,10 +19,9 @@ function loadUsers() {
 function fetchGet(url) {
     return fetch(url)
         .then(resp => {
-            if(resp.ok) {
+            if (resp.ok) {
                 return resp.json();
             }
-
             return Promise.reject(resp);
         });
 }
@@ -31,7 +32,55 @@ function insertUsers(usersList) {
     usersList.forEach(user => {
         const liElement = document.createElement('li');
         liElement.innerText = `${user.firstName} ${user.lastName}`;
-
         ulElement.appendChild(liElement);
     });
+}
+
+
+function initAddUserForm() {
+    const form = document.querySelector('.form');
+    form.addEventListener('submit', handleSubmit);
+}
+
+function handleSubmit(e) {
+    e.preventDefault();
+
+    const firstNameInput = document.querySelector('.form__field--first-name');
+    const lastNameInput = document.querySelector('.form__field--last-name');
+
+    const firstName = firstNameInput.value.trim();
+    const lastName = lastNameInput.value.trim();
+
+    if (!firstName || !lastName) {
+        alert('Uzupełnij oba pola!');
+        return;
+    }
+
+    const user = {
+        firstName,
+        lastName
+    };
+
+    
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+        .then(resp => {
+            if (!resp.ok) {
+                return Promise.reject(resp);
+            }
+            return resp.json();
+        })
+        .then(() => {
+            firstNameInput.value = '';
+            lastNameInput.value = '';
+        })
+        .catch(err => console.error('Błąd przy dodawaniu użytkownika:', err))
+        .finally(() => {
+            loadUsers();
+        });
 }
